4b9a3d149c3d45a3266433ec2e589022ea3166dc
Lewis Alexis Rengifo Pizango <l.rengifo@pucp.edu.pe>
2025-08-21 20:51:26 -0500

    feat: log

--- src/main/java/com/pucp/lab020252/controller/PetController.java
+++ src/main/java/com/pucp/lab020252/controller/PetController.java
@@ -1,12 +1,15 @@
 package com.pucp.lab020252.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class PetController {
     @GetMapping("/pets")
     public String getPets() {
+        log.info("Pet controller accessed");
         return "pets";
     }
 }
