315bb6e5dc1e99aec035c17a57aadcf92d6f639b
Lewis Alexis Rengifo Pizango <l.rengifo@pucp.edu.pe>
2025-08-21 20:25:32 -0500

    feat: imprimiendo lista

--- src/main/java/com/pucp/lab020252/Lab020252Application.java
+++ src/main/java/com/pucp/lab020252/Lab020252Application.java
@@ -3,11 +3,17 @@ package com.pucp.lab020252;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class Lab020252Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Lab020252Application.class, args);
     System.out.println("Hola Mundo!");
+    List<String> lista = List.of("Elemento1", "Elemento2", "Elemento3");
+    for (String s : lista) {
+      System.out.println("elemento: " + s);
+    }
   }
 }
