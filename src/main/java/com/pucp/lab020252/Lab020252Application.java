ce61a57b991cb0e898f423b10a8c06fba5302993
Lewis Alexis Rengifo Pizango <l.rengifo@pucp.edu.pe>
2025-08-21 21:01:34 -0500

    feat: rama develop

--- src/main/java/com/pucp/lab020252/Lab020252Application.java
+++ src/main/java/com/pucp/lab020252/Lab020252Application.java
@@ -17,5 +17,6 @@ public class Lab020252Application {
     }
     System.out.println("API para creacion de mascotas");
     System.out.println("Nuevo cambio");
+    System.out.println("rama develop!!");
   }
 }
