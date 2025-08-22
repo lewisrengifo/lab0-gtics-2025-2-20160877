a72e3efd939db5cb38a0f85ce3aea1e30be67b75
Lewis Alexis Rengifo Pizango <l.rengifo@pucp.edu.pe>
2025-08-21 20:52:05 -0500

    feat: lista

--- src/main/java/com/pucp/lab020252/controller/PetController.java
+++ src/main/java/com/pucp/lab020252/controller/PetController.java
@@ -3,10 +3,13 @@ package com.pucp.lab020252.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class PetController {
     @GetMapping("/pets")
     public String getPets() {
+        List<String> pets = List.of("Dog", "Cat", "Bird");
         return "pets";
     }
 }
